51ac5258e2558e093b1607f1537a6c7562134dca
Robert Wagner <rwwagner90@gmail.com>
2018-01-30 18:38:24 -0500

    Use just fetch listener

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,6 +1,5 @@
-
 import {
-  URLS,
+  PREMBER_URLS,
   VERSION
 } from 'ember-service-worker-prember/service-worker/config';
 
@@ -10,31 +9,29 @@ import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 const CACHE_KEY_PREFIX = 'esw-prember';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
 
-URLS.forEach((url) => {
-  const INDEX_HTML_URL = new URL(url, self.location).toString();
+self.addEventListener('fetch', (event) => {
+  let request = event.request;
+  let isGETRequest = request.method === 'GET';
+  let isHTMLRequest = request.headers.get('accept').indexOf('text/html') !== -1;
+  let isLocal = new URL(request.url).origin === location.origin;
 
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      fetch(INDEX_HTML_URL, { credentials: 'include' }).then((response) => {
-        return caches
-          .open(CACHE_NAME)
-          .then((cache) => cache.put(INDEX_HTML_URL, response));
+  if (isGETRequest && isHTMLRequest && isLocal && request.url.endsWith('/')) {
+    event.respondWith(
+      caches.match(request, { cacheName: CACHE_NAME }).then((cached) => {
+        return cached || fetch(event.request)
+        .then((response) => {
+          const cacheCopy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function add(cache) {
+              cache.put(event.request, cacheCopy);
+            });
+
+          return response;
+        });
       })
     );
-  });
-
-  self.addEventListener('fetch', (event) => {
-    let request = event.request;
-    let isGETRequest = request.method === 'GET';
-    let isHTMLRequest = request.headers.get('accept').indexOf('text/html') !== -1;
-    let isLocal = new URL(request.url).origin === location.origin;
-
-    if (isGETRequest && isHTMLRequest && isLocal) {
-      event.respondWith(
-        caches.match(INDEX_HTML_URL, { cacheName: CACHE_NAME })
-      );
-    }
-  });
+  }
 });
 
 self.addEventListener('activate', (event) => {
