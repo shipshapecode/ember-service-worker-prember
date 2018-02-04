87cf617fb6149a68a5e4f675a4a96148fae4cacd
Robert Wagner <rwwagner90@gmail.com>
2018-02-04 01:39:39 -0500

    Break into functions

--- service-worker/index.js
+++ service-worker/index.js
@@ -9,21 +9,47 @@ import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 const CACHE_KEY_PREFIX = 'esw-prember';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
 
-self.addEventListener('fetch', (event) => {
-  let request = event.request;
-  let url = request.url;
+/**
+ * Check if this is a GET request, the type is HTML, and it is on our local origin.
+ * @param request The request object
+ * @param {string} url The request.url string
+ * @returns {boolean} True if this is a GET request, the type is HTML, and on our origin
+ */
+function isLocalHTMLGETRequest(request, url) {
   let isGETRequest = request.method === 'GET';
   let isHTMLRequest = request.headers.get('accept').indexOf('text/html') !== -1;
   let isLocal = new URL(url).origin === location.origin;
 
-  // Check if the path we are visiting is in the prember routes with or without trailing slash
+  return isGETRequest && isHTMLRequest && isLocal;
+}
+
+/**
+ * Check if the path we are visiting is in the prember routes with or without trailing slash
+ * @param {string} url The url to check
+ * @returns {boolean} Whether this is a prember url or not
+ */
+function isPremberURL(url) {
   const withSlash = url.endsWith('/') ? url : `${url}/`;
   const withoutSlash = url.endsWith('/') ? url.slice(0, -1) : url;
-  const isPremberURL =
-    PREMBER_URLS.indexOf(new URL(withSlash).pathname) !== -1 ||
+  return PREMBER_URLS.indexOf(new URL(withSlash).pathname) !== -1 ||
     PREMBER_URLS.indexOf(new URL(withoutSlash).pathname) !== -1;
+}
 
-  if (isGETRequest && isHTMLRequest && isLocal && isPremberURL) {
+/**
+ * Cleanup old cached content
+ */
+self.addEventListener('activate', (event) => {
+  event.waitUntil(cleanupCaches(CACHE_KEY_PREFIX, CACHE_NAME));
+});
+
+/**
+ * Fetch the page and only cache the static HTML
+ */
+self.addEventListener('fetch', (event) => {
+  let request = event.request;
+  let url = request.url;
+
+  if (isLocalHTMLGETRequest(request, url) && isPremberURL(url)) {
     event.respondWith(
       caches.match(url, { cacheName: CACHE_NAME }).then((cached) => {
         return cached || fetch(event.request)
@@ -42,8 +68,4 @@ self.addEventListener('fetch', (event) => {
       })
     );
   }
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(cleanupCaches(CACHE_KEY_PREFIX, CACHE_NAME));
 });
